2a20472bd5e2f8d783fbfb9de2378ee97cbbcabf
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2023-06-02 19:33:51 +0100

    feat: break out isterminal! and POMDPAdaptors.basicreward!

--- src/POMDPAdaptors.jl
+++ src/POMDPAdaptors.jl
@@ -11,15 +11,49 @@ using QuickPOMDPs
 using PandemicAIs
 import PandemicAIs: Actions
 
+"""
+    basicreward!(state, previousaction; default=1.0, pass=0.0, loss=-100.0, win=100.0)
+
+Simple function for rewards.
+`state` is the game state which was obtained through `previousaction`.
+This function must modify `state` because [`Pandemic.checkstate!`](@ref) does.
+
+Allows passing reward amounts in, which are returned based on the state of the game:
+- `win` and `loss` for the respective game ends
+- `pass` if `previousaction` is [`Actions.Pass`](@ref); intended to de-incentivise doing nothing
+- `default` if none of the above apply
+"""
+function basicreward!(
+    state,
+    prevaction;
+    default = 1.0,
+    pass = 0.0,
+    loss = -100.0,
+    win = 100.0,
+)::Float64
+    gs = Pandemic.checkstate!(state)
+
+    if state == Pandemic.Lost
+        return loss
+    end
+    if state == Pandemic.Won
+        return win
+    end
+    if prevaction == Actions.Pass
+        return pass
+    end
+    return default
+end
+
+
 function getquickmdp()
     # TODO: use the `_rng` arg
     function genfunc(cur, act, _rng)
         next = Actions.resolveandbranch(cur, act)[1]
-        # De-incentivise doing nothing
-        reward = if act == Actions.Pass 0.0 else 1.0 end
+        reward = basicreward!(next, act)
         (sp = next, r = reward)
     end
-    isterminal = (state) -> Pandemic.checkstate!(state) != Pandemic.Playing
+    isterminal = PandemicAIs.isterminal!
 
     QuickMDP(
         genfunc,
--- src/PandemicAIs.jl
+++ src/PandemicAIs.jl
@@ -10,6 +10,15 @@ export Pandemic
 
 using PrecompileTools
 
+"""
+    isterminal!(game)
+
+Checks if `game` is in a terminal state, with [`Pandemic.checkstate!`](@ref).
+"""
+function isterminal!(game)::Bool
+    Pandemic.checkstate!(game) != Pandemic.Playing
+end
+
 @compile_workload begin
     map = Pandemic.Maps.vanillamap()
     g = Pandemic.newgame(map, Pandemic.Introductory, 4)
