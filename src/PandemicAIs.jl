14563f96be49cda04691964044e6aedc99504b9f
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2023-06-02 16:23:59 +0100

    feat: add precompilation stuff

--- src/PandemicAIs.jl
+++ src/PandemicAIs.jl
@@ -7,4 +7,14 @@ using .Actions
 using Pandemic
 export Pandemic
 
+using PrecompileTools
+
+@compile_workload begin
+    map = Pandemic.Maps.vanillamap()
+    g = Pandemic.newgame(map, Pandemic.Introductory, 4)
+    for act in Actions.possibleactions(g)
+        branch = Actions.resolveandbranch(g, act)
+    end
+end
+
 end
